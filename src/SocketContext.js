8e15484e927822550e67a472c9e0b1e6a20d957a
tushar1527 <tushar.savaliya@centillionsofttech.com>
2022-09-10 11:54:20 +0530

    update changes

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://139.59.75.48:8001");
+const socket = io("https://my-vicearmory.tk:8001");
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
--- src/pages/socket.js
+++ src/pages/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const socket = socketIOClient("http://139.59.75.48:8000", {
+const socket = socketIOClient("https://my-vicearmory.tk:8000", {
   extraHeaders: {
     "Access-Control-Allow-Origin": "*",
   },
